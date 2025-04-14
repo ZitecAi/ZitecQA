25b432e664b92b77d287b0c7894f0d72d6d29146
Jéssica Tavares <jessica.vitoria.tavares044@gmail.com>
2025-04-14 11:09:33 -0300

    Adicionado TestePortal
    Adicionado automação de testes do portal IDSF

---
+++ TestePortal/Model/Operacoes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestePortal.Model
+{
+    public class Operacoes
+    {
+        public string NovoNomeArquivo { get; set; }
+
+        public string StatusTrocados { get; set; }
+
+        public int totalErros { get; set; }
+
+        public string AprovacoesRealizadas { get; set; }
+
+        public List<string> ListaErros { get; set; }
+        public Operacoes()
+        {
+            ListaErros = new List<string>();
+        }
+
+    }
+}
---
+++ TestePortal/Utils/Service.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TestePortal.Utils
+{
+    public static class Service
+    {
+        
+        public static class LogRequestService
+        {
+         
+            public static void Add(string apiUrl, string param1, string param2, string responseBody, string method, string status, DateTime date, string source)
+            {
+                try
+                {
+          
+                    string logFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "log.txt");
+                    string logEntry = $"[{date}] - API: {apiUrl}, Method: {method}, Status: {status}, Params: ({param1}, {param2}), Response: {responseBody}, Source: {source}\n";
+                    File.AppendAllText(logFilePath, logEntry);
+                }
+                catch (Exception ex)
+                {
+                   
+                    Console.WriteLine($"Erro ao gravar log: {ex.Message}");
+                }
+            }
+        }
+    }
+}
