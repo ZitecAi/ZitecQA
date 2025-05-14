9d9ed5cb855143c22fbbf386533330a5890b247c
Jéssica Tavares <jessica.vitoria.tavares044@gmail.com>
2025-05-14 16:50:46 -0300

    Fix -
    Adicionando bot que verifica se o bot de comunicações respondeu no slack

---
+++ EnviarMensagemSlack/Program.cs
@@ -0,0 +1,22 @@
+﻿using EnviarMensagemSlack.Utils;
+using Microsoft.Graph.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+
+namespace EnviarMensagemSlack
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await Utils.EnviarMensagemSlack.MandarMsgFundoQaAsync();
+        }
+    }
+}
