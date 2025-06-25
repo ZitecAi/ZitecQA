759ccd90d549c04d7fb22e65f3520a33dd6fe3b8
Levi Alves <alveslevi@icloud.com>
2025-06-25 10:01:31 -0300

    wip - Adição das ultimas páginas ao robô

--- TestePortal/Pages/BoletagemPage/ContaOrdem.cs
+++ TestePortal/Pages/BoletagemPage/ContaOrdem.cs
@@ -30,7 +30,7 @@ namespace TestePortal.Pages.BoletagemPage
                     pagina.Nome = "Conta e Ordem";
                     pagina.StatusCode = controleCapital.Status;
                     pagina.Reprovar = "❓";
-                    pagina.Acentos = Utils.Acentos.ValidarAcentos(Page).Result;                    
+                    pagina.Acentos = Utils.Acentos.ValidarAcentos(Page).Result;
                     pagina.BaixarExcel = "❓";
 
                     if (pagina.Acentos == "❌")
@@ -44,29 +44,78 @@ namespace TestePortal.Pages.BoletagemPage
                     }
                     //await Page.PauseAsync();
                     await Page.GetByRole(AriaRole.Button, new() { Name = "+ Novo" }).ClickAsync();
-                    await Page.Locator("#selectFundo").SelectOptionAsync(new[] { "53300608000106" });
-                    await Page.Locator("#selectCarteira").SelectOptionAsync(new[] { "777777" });
+                    await Page.Locator("#selectFundo").SelectOptionAsync(new[] { "54638076000176" });
+                    await Page.Locator("#selectCarteira").SelectOptionAsync(new[] { "791348654" });
                     await Page.Locator("#inputDistribuidor").ClickAsync();
-                    await Page.Locator("#inputDistribuidor").FillAsync("QA");
-                    await Page.GetByLabel("Novo Registro de Conta/Ordem").Locator("i").ClickAsync();
-                    await Page.GetByLabel("Novo Registro de Conta/Ordem").SetInputFilesAsync(new[] { "arquivoTest.csv" });
+                    await Page.Locator("#inputDistribuidor").FillAsync("TesteQA");
+                    //await Page.GetByLabel("Novo Registro de Conta/Ordem").Locator("i").ClickAsync();
+                    await Page.Locator("#inputEnviarOrdem").SetInputFilesAsync(new[] { TestePortalIDSF.Program.Config["Paths:Arquivo"] + "caixa_CONTAORDEM (1).csv" });
                     await Page.GetByRole(AriaRole.Button, new() { Name = "Importar" }).ClickAsync();
+                    //await Page.GetByText("Ordem enviada com sucesso!").ClickAsync();
+                    var contaEscrowCriada = await Page.WaitForSelectorAsync("text=Ordem enviada com sucesso!", new PageWaitForSelectorOptions
+                    {
+
+                        Timeout = 90000
+
+                    });
 
                     //Fazer Fluxo de inserir dados e criar repository
+                    var contaOrdemExiste = Repository.BoletagemContaOrdem.BoletagemContaOrdemRepository.VerificaExistenciaBoletagemContaOrdem("qazitec01@gmail.com", "TesteQA");
+
+                    if (contaOrdemExiste)
+                    {
+                        Console.WriteLine("Conta Ordem adicionado com sucesso na tabela.");
+                        pagina.InserirDados = "✅";
+                        var apagarContaOrdem = Repository.BoletagemContaOrdem.BoletagemContaOrdemRepository.ApagarBoletagemContaOrdem("qazitec01@gmail.com", "TesteQA");
+
+                        if (apagarContaOrdem)
+                        {
+                            Console.WriteLine("Conta Ordem apagado com sucesso");
+                            var apagarAporteBoletagemContaOrdem = Repository.BoletagemContaOrdem.BoletagemContaOrdemRepository.ApagarAporteBoletagemContaOrdem(2929);
+                            if (apagarAporteBoletagemContaOrdem)
+                            {
+                                Console.WriteLine("Aporte Conta Ordem apagado com sucesso");
+                                pagina.Excluir = "✅";
+                            }
+                            else
+                            {
+                                Console.WriteLine("Não foi possivel apagar Aporte da conta ordem");
+                                pagina.Excluir = "❌";
+                            }
+                                Console.WriteLine("Conta Ordem apagado com sucesso");
+                            var apagarResgateBoletagemContaOrdem = Repository.BoletagemContaOrdem.BoletagemContaOrdemRepository.ApagarResgateBoletagemContaOrdem("qazitec01@gmail.com");
+                            if (apagarAporteBoletagemContaOrdem)
+                            {
+                                Console.WriteLine("Resgate Conta Ordem apagado com sucesso");
+                                pagina.Excluir = "✅";
+                            }
+                            else
+                            {
+                                Console.WriteLine("Não foi possivel apagar Resgate da conta ordem");
+                                pagina.Excluir = "❌";
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Não foi possível apagar Conta Ordem");
+                            pagina.Excluir = "❌";
+                            errosTotais++;
+                        }
+                        
 
 
-
+                    }
+                    else
+                    {
+                        Console.Write("Erro ao carregar a página de Conta e Ordem no tópico Boletagem ");
+                        pagina.Nome = "Conta e Ordem";
+                        pagina.StatusCode = controleCapital.Status;
+                        errosTotais++;
+                        await Page.GotoAsync("https://portal.idsf.com.br/Home.aspx");
+                    }
                 }
-                else
-                {
-                    Console.Write("Erro ao carregar a página de Conta e Ordem no tópico Boletagem ");
-                    pagina.Nome = "Conta e Ordem";
-                    pagina.StatusCode = controleCapital.Status;
-                    errosTotais++;
-                    await Page.GotoAsync("https://portal.idsf.com.br/Home.aspx");
-                }
+
             }
-
             catch (Exception ex)
             {
                 Console.WriteLine("exceção lançada");
