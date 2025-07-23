1003eb052853267aa10d4e9141debf0bde9646cd
Levi Alves <alveslevi@icloud.com>
2025-07-23 17:54:39 -0300

    fix - Manutenção no usuario gestora, e adição de link staging

--- TestePortal/Utils/Util.cs
+++ TestePortal/Utils/Util.cs
@@ -16,7 +16,7 @@ namespace TestePortal.Utils
 
             usuarios.Add(new Usuario("qazitec01@gmail.com", "Testeqa01?!", Usuario.NivelEnum.Master));
             usuarios.Add(new Usuario("jessica.tavares@aluno.ifsp.edu.br", "Jehtavares?123", Usuario.NivelEnum.Consultoria));
-            usuarios.Add(new Usuario("jt@zitec.ai", "Jehtavares?123", Usuario.NivelEnum.Gestora));
+            usuarios.Add(new Usuario("caiooliweira@gmail.com", "id2021", Usuario.NivelEnum.Gestora));
             usuarios.Add(new Usuario("jessica.vitoria.tavares044@gmail.com", "Jehtavares?123", Usuario.NivelEnum.Denver));
             return usuarios;
         }
