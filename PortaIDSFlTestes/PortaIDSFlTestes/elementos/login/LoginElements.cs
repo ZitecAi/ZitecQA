7c13554eec7dedc2019e5b12c1c3807581b063b1
Levi Alves <alveslevi@icloud.com>
2025-07-25 15:20:38 -0300

    Nova solução de testes

---
+++ PortaIDSFlTestes/PortaIDSFlTestes/elementos/login/LoginElements.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortaIDSFTestes.elementos.login
+{
+    public class LoginElements
+    {
+
+        public string campoEmail { get;  } = "#email";
+        public string campoSenha { get; } = "#password";
+        public string loginBtn { get; } = "//button[text()='Entrar']";
+
+        public string errorMessage { get; } = "#erroraccess";
+
+
+
+
+    }
+}
---
+++ PortaIDSFlTestes/PortaIDSFlTestes/testes/operacoes/ArquivosBaixaTest.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Playwright;
+using PortaIDSFTestes.pages.login;
+using PortaIDSFTestes.pages.operacoes;
+using PortaIDSFTestes.runner;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortaIDSFTestes.testes.operacoes
+{
+    [Parallelizable(ParallelScope.Self)]
+    [TestFixture]
+    [Category("ArquivosBaixaTest")]
+    public class ArquivosBaixaTest : Executa
+    {
+        private IPage page;
+
+        [SetUp]
+        public async Task SetUp()
+        {
+            page = await AbrirBrowserAsync();
+            var login = new LoginPage(page);
+            await login.LoginSucessoInterno();
+        }
+
+        [TearDown]
+        public async Task TearDown()
+        {
+            await FecharBrowserAsync();
+        }
+
+        [Test, Order(1)]
+        public async Task naoDeveConterAcentosQuebrados()
+        {
+            var baixa = new ArquivosBaixaPage(page);
+            await baixa.validarAcentosArquivosBaixaPage();
+        }
+
+        [Test, Order(2)]
+        public async Task deveEnviarArquivoBaixa()
+        {
+            var baixa = new ArquivosBaixaPage(page);
+            await baixa.enviarArquivoBaixa();
+        }
+
+
+
+    }
+}
