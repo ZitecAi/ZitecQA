50cfc70c87a81dece6396abf0a9a44ba956bbe41
Levi Alves <alveslevi@icloud.com>
2025-09-15 15:40:19 -0300

    Wip carteira validations

---
+++ PortalIDSFTestes/elementos/cadastro/CarteirasElements.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortalIDSFTestes.elementos.cadastro
+{
+    public class CarteirasElements
+    {
+        public string MenuCadastro { get; } = "//p[text()='Cadastro']";
+        public string PaginaCarteiras { get; } = "//p[text()='Carteiras']";
+
+
+
+    }
+}
---
+++ PortalIDSFTestes/pages/cadastro/CarteirasPage.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Playwright;
+using PortalIDSFTestes.elementos.cadastro;
+using PortalIDSFTestes.metodos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortalIDSFTestes.pages.cadastro
+{
+    public class CarteirasPage
+    {
+
+        private readonly IPage page;
+        Metodos metodo;
+        CarteirasElements el = new CarteirasElements();
+
+        public CarteirasPage(IPage page)
+        {
+            this.page = page;
+            metodo = new Metodos(page);
+        }
+
+
+        public async Task ValidarAcentosCarteiras()
+        {
+            await metodo.ValidarAcentosAsync(page, "Validar Acentos na Página Carteiras");
+        }
+
+    }
+}
