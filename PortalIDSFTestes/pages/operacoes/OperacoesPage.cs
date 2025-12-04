3a3996f8fbb05a5d4ede1549675e6a110aef6898
Levi Alves <alveslevi@icloud.com>
2025-12-04 15:38:15 -0300

    Fix - Download excel add delay

--- PortalIDSFTestes/pages/operacoes/OperacoesPage.cs
+++ PortalIDSFTestes/pages/operacoes/OperacoesPage.cs
@@ -182,7 +182,7 @@ namespace PortalIDSFTestes.pages.operacoes
 
         public async Task DownloadExcel()
         {
-
+            await Task.Delay(10000);
             await metodo.ValidateDownloadAndLength(page, el.BtnDownloadExcel, "Validar Download do Excel na página de Operações");
         }
 
